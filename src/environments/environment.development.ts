f9e44eb0504eaade32da3f9576372f614255090e
PeHachePe <pehachepe@uoc.edu>
2024-04-15 00:05:50 +0200

    fix api url

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://superheroes-one.vercel.app/superheroes',
+  apiUrl: 'https://superheroes-one.vercel.app',
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://superheroes-one.vercel.app/superheroes',
+  apiUrl: 'http://superheroes-one.vercel.app',
 };
