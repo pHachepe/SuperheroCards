02083828c957294a99c65787ce8ed5b3d862d23d
Abraham <afontana@unedbarbastro.es>
2024-04-15 12:55:04 +0200

    Del Css

--- src/app/modules/superheroes/components/create-superhero/create-superhero.component.ts
+++ src/app/modules/superheroes/components/create-superhero/create-superhero.component.ts
@@ -3,8 +3,5 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-create-superhero',
   templateUrl: './create-superhero.component.html',
-  styleUrls: ['./create-superhero.component.css']
 })
-export class CreateSuperheroComponent {
-
-}
+export class CreateSuperheroComponent {}
--- src/app/modules/superheroes/components/edit-superhero/edit-superhero.component.ts
+++ src/app/modules/superheroes/components/edit-superhero/edit-superhero.component.ts
@@ -3,8 +3,5 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-edit-superhero',
   templateUrl: './edit-superhero.component.html',
-  styleUrls: ['./edit-superhero.component.css']
 })
-export class EditSuperheroComponent {
-
-}
+export class EditSuperheroComponent {}
