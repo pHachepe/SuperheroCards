b2ca3b7b496919517182b373f93ccaf1a7009f9e
Abraham <afontana@unedbarbastro.es>
2024-04-22 14:04:14 +0200

    Add ConfirmDialogComponent to shared module

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,11 +1,14 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDialogModule } from '@angular/material/dialog';
+import { ConfirmDialogComponent } from './components/confirm-dialog/confirm-dialog.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { GenderIconPipe } from './pipes/gender-icon/gender-icon.pipe';
 
 @NgModule({
-  declarations: [GenderIconPipe, FooterComponent],
-  imports: [CommonModule],
-  exports: [GenderIconPipe, FooterComponent],
+  declarations: [GenderIconPipe, FooterComponent, ConfirmDialogComponent],
+  imports: [CommonModule, MatDialogModule, MatButtonModule],
+  exports: [GenderIconPipe, FooterComponent, ConfirmDialogComponent],
 })
 export class SharedModule {}
